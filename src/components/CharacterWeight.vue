3dcabc57e146012c336cbf09e1cc7274401e200e
Antidale <antidale@tellah.life>
2026-04-21 22:38:33 -0700

    basic create vue@latest, some of demo app ripped out

---
+++ src/components/CharacterWeight.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+//should communicate with the parent for the weight/range given for the component
+</script>
+
+<template>
+    <h1></h1>
+    <!-- 
+     have an image of the character
+     have either an input box or a slider or something to say how often the character should appear
+        0 = never allow rolling
+        to 
+        100 (?) or some max, but this number doesn't really matter, would just be for relative scaling vs the other characters
+     -->
+</template>
