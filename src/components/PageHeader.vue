45a6d3fdbca4719fb1f98ef00a6287f0f6f7772d
Antidale <antidale@tellah.life>
2026-05-01 13:36:17 -0700

    actually add header comp, dark mode color updates

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header class="page-header">
+      <h1 class="center">STS 2 Character Selector</h1>
+    </header>
+</template>
+
+<style lang="css" scoped>
+  h1 {
+    border: 1px solid var(--color);
+    margin-top: .5rem;
+    margin-left: 20%;
+    margin-right: 20%;
+    border-radius: 100px;
+    padding: .5rem;
+    box-shadow: .1rem .1rem .4rem .1rem oklch(from var(--color) l c h / calc(alpha - .6));
+  }
+</style>
