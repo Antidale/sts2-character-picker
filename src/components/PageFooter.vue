28faf7e0b3f6e60322d25ad034887bd39803b243
Antidale <antidale@tellah.life>
2026-05-01 17:19:31 -0700

    minor light css update, include page footer file

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <footer class="page-footer">
+      <p class="center"><a href="https://github.com/Antidale/sts2-character-picker">Source</a></p>
+    </footer>
+</template>
+
+<style lang="css" scoped>
+    a {
+        text-decoration: none;
+    }
+</style>
