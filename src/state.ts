961d762657108d069f7e10f0b45ea726f5ee01ec
Antidale <antidale@tellah.life>
2026-05-07 16:03:46 -0700

    functioning basic settings

--- src/state.ts
+++ src/state.ts
@@ -1,9 +1,28 @@
 import { reactive, ref } from 'vue';
 
-export const Store = reactive({
+const ironclad = "Ironclad"
+const silent = "Silent"
+const regent = "Regent"
+const necrobinder = "Necrobinder"
+const defect = "Defect"
+
+const Store = reactive({
     ironCladModel: ref(20),
     silentModel: ref(20),
     regentModel: ref(20),
     necrobinderModel: ref(20),
-    defectModel: ref(20),
+    defectModel: ref(20)
 })
+
+const Characters = {
+    ironclad: ironclad,
+    silent: silent,
+    regent: regent,
+    necrobinder: necrobinder,
+    defect: defect,
+    characterNames: [ironclad, silent, regent, necrobinder, defect]
+}
+
+export { Store, Characters };
+
+
